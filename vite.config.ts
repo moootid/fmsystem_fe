e628e08dd0b19543701670084f04a71596e00f5b
Mohammed Almalki <mr.motid@gmail.com>
2025-05-04 03:52:23 +0300

    Chore: Load environment variables in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
-
+import * as dotenv from 'dotenv';
+import * as dotenvExpand from 'dotenv-expand';
+function loadEnv() {
+  var myEnv = dotenv.config();
+  dotenvExpand.expand(myEnv);
+}
+loadEnv();
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  define: {
+    'process.env': process.env,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
